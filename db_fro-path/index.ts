1a549fa79157b3f58bfdd9744a7d2e8be796ff34
HernandezVicente <v.hernandez09@ufromail.cl>
2026-05-13 11:12:50 -0400

    primer commit para la base de datos
    se agregan la configuración inicial para la base de datos, recordar que antes de ejecutar tiene que estar activa la base de datos

---
+++ db_fro-path/index.ts
@@ -0,0 +1,7 @@
+import { AppDataSource } from "./data-source.js";
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("¡Conexión exitosa a la base de datos! 🚀");
+    })
+    .catch((error) => console.log("Error durante la conexión: ", error));

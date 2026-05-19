2bf0a7966abf2b2e34dfc9e25d584e4d277a71f3
HernandezVicente <v.hernandez09@ufromail.cl>
2026-05-18 22:10:24 -0400

    Delete db_fro-path directory
    se borra este directorio, porque ya había un directorio para la base de datos, es se ubica en ./packages/database

--- db_fro-path/index.ts
+++
@@ -1,7 +0,0 @@
-import { AppDataSource } from "./data-source.js";
-
-AppDataSource.initialize()
-    .then(() => {
-        console.log("¡Conexión exitosa a la base de datos! 🚀");
-    })
-    .catch((error) => console.log("Error durante la conexión: ", error));

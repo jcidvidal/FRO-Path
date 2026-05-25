83ece79437d452af70e582335e553b615e4df983
Maximiliano <m.rivas15@ufromail.cl>
2026-05-25 16:25:52 -0400

    feature: clases y sus respectivos metodos agregados

---
+++ apps/server/src/modules/progress/domain/entities/profesor.entity.ts
@@ -0,0 +1,15 @@
+import { Asignatura } from './asignatura.entity';
+import { Usuario } from './usuario.entity';
+
+export class Profesor extends Usuario {
+  constructor(
+    id: number,
+    nombre: string,
+    correo: string,
+    rut: string,
+    contrasena: string,
+    public asignaturas: Asignatura[] = []
+  ) {
+    super(id, nombre, correo, rut, contrasena);
+  }
+}

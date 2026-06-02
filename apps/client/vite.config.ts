b4458a21fbf634c67884fb57f51133bffcf276d9
06-Benjamin-V <b.valenzuela06@ufromail.cl>
2026-06-02 09:45:29 -0400

    feat: agregar configuraciones para sonaqube y la cobertura de pruebas

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,10 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test:{
-    globals:true,
-    environment:'jsdom',
+  test: {
+    globals: true,
+    environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
     css: true,
+    include: ['**/*.test.tsx', '**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov', 'text'],
+      reportsDirectory: './coverage',
+      exclude: [
+        'node_modules/**',
+        'src/test/**',
+        '**/*.d.ts',
+        'src/main.tsx',
+        'vite.config.ts'
+      ]
+    }
   }
 })

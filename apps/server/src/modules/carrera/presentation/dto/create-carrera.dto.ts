31fdc8a3f593b996aab4c9248cc3c03e76f66345
Juan Pablo Cid <j.cid12@ufromail.cl>
2026-06-26 01:26:56 -0400

    feat(carrera): conexion a la malla de ingenieria informatica y civil

--- apps/server/src/modules/carrera/presentation/dto/create-carrera.dto.ts
+++ apps/server/src/modules/carrera/presentation/dto/create-carrera.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateCarreraDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre es obligatorio.' })
   nombre!: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'El codigo de carrera es obligatorio.' })
   codigo_carrera!: string;
 }
